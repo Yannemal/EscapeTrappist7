322bc5ae3c2fd6b192f02de354c02f301fc40c1b
yannemal <56878180+Yannemal@users.noreply.github.com>
2023-04-28 22:22:56 +0800

    initial set up shooter game
    boiler plate spritekit and some sprites

---
+++ StarshipShooter/StarshipShooter/Scenes/GameScene.swift
@@ -0,0 +1,50 @@
+//  GameViewController.swift
+//  StarshipShooter: "Escape From Trappist 7"
+//  tutorial Jack iLy https://www.youtube.com/watch?v=Q1C-M0MaPM4
+//  Created by yannemal on 28/04/2023.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+   
+    
+    override func sceneDidLoad() {
+
+       
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+       
+        
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+       
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+     
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        
+        }
+        
+        // Calculate time since last update
+      
+        
+        // Update entities
+        
+        
+      
+    }
+
